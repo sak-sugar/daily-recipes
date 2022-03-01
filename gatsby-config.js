b06be3f74ac572529e378dc4fa7193cd6411572b
sak-sugar <kawamura.s.sak@gmail.com>
2022-03-01 21:48:36 +0900

    add: microCMS setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,48 @@
 /** @type {import('gatsby').GatsbyConfig} */
+
+const path = require('path');
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
 module.exports = {
   siteMetadata: {
       title: ``,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image", 
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
     }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  },
+  "gatsby-plugin-sharp",
+  "gatsby-transformer-sharp",
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  }]
+  },
+  // microCMS
+  {
+    resolve: "gatsby-source-microcms",
+    options: {
+      apiKey: process.env.API_KEY,
+      serviceId: process.env.SERVICE_ID,
+      apis: [
+        {
+          endpoint: "listgatsby-config.js",
+        },
+      ],
+    },
+  },
+]
 };
